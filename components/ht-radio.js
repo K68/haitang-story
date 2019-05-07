7d4454dc058a204cfa9e594d89a3b63a82144a25
mxh <1075018028@qq.com>
2019-05-07 17:50:42 +0800

    ...

--- components/ht-radio.js
+++ components/ht-radio.js
@@ -15,13 +15,13 @@ class HtRadio extends React.Component {
         <div {...props} style={{ margin: '8px 0' }}>
           <div>
             {options.map((item, index) => (typeof item === 'object' ?
-              <div key={item.value + index} style={{ width: '50%', display: 'inline-block' }}>
+              <div key={item.value + index} style={{ width: '50%', display: 'inline-block', textAlign: 'center' }}>
                   <input onChange={this.increment.bind(this)} type="radio" name={`radio${step}`} checked={value === item.value} value={item.value} />
                   <label><span><span></span></span>{item.value}</label>
-                  {item.img ? <img onClick={this.increment.bind(this)} data-value={item.value} style={{ maxWidth: 64, maxHeight: 64, margin: 0, paddingLeft: 24 }} src={item.img} alt="" /> : null}
+                  {item.img ? <img onClick={this.increment.bind(this)} data-value={item.value} style={{ maxWidth: 64, maxHeight: 64, margin: 0, display: 'inline-block' }} src={item.img} alt="" /> : null}
               </div>
             :
-              <div key={item + index}>
+              <div key={item + index} style={{ width: '50%', display: 'inline-block', textAlign: 'center' }}>
                 <input onChange={this.increment.bind(this)} type="radio" name={`radio${step}`} value={item} /><label><span><span></span></span>{item}</label>
               </div>
             ))}
