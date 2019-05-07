61a31ee2b6393cb1a5f9124610729e8ec4f1111c
mxh <1075018028@qq.com>
2019-05-07 16:56:33 +0800

    ...

--- components/ht-radio.js
+++ components/ht-radio.js
@@ -2,24 +2,27 @@ const React = require('react');
 
 class HtRadio extends React.Component {
     increment(e) {
+        let stepSitu = 0;
+        stepSitu = this.props.step > this.props.stepSitu? this.props.step : this.props.stepSitu;
         this.props.updateProps({
             value: e.target.value || e.target.getAttribute('data-value'),
+            stepSitu: stepSitu,
         });
     };
   render() {
-    const { hasError, idyll, updateProps, options, value, ...props } = this.props;
+    const { hasError, idyll, updateProps, options, value, stepSitu, step,   ...props } = this.props;
     return (
         <div {...props} style={{ margin: '8px 0' }}>
           <div>
             {options.map((item, index) => (typeof item === 'object' ?
-              <div key={item.label + index} style={{ width: '50%', display: 'inline-block' }}>
-                  <input onClick={this.increment.bind(this)} type="radio" name="radio" checked={value === item.value} value={item.value} />
-                  <label><span><span></span></span>{item.label}</label>
+              <div key={item.value + index} style={{ width: '50%', display: 'inline-block' }}>
+                  <input onChange={this.increment.bind(this)} type="radio" name={`radio${step}`} checked={value === item.value} value={item.value} />
+                  <label><span><span></span></span>{item.value}</label>
                   {item.img ? <img onClick={this.increment.bind(this)} data-value={item.value} style={{ maxWidth: 64, maxHeight: 64, margin: 0, paddingLeft: 24 }} src={item.img} alt="" /> : null}
               </div>
             :
               <div key={item + index}>
-                <input onClick={this.increment.bind(this)} type="radio" name="radio" value={item} /><label><span><span></span></span>{item}</label>
+                <input onChange={this.increment.bind(this)} type="radio" name={`radio${step}`} value={item} /><label><span><span></span></span>{item}</label>
               </div>
             ))}
           </div>
