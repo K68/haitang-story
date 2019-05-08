cace40131b620dbfc624dfef1855e2491163a78f
zxyao <1158396760@qq.com>
2019-05-08 17:05:23 +0800

    提交和统计按钮

---
+++ components/ht-submit.js
@@ -0,0 +1,50 @@
+const React = require('react');
+
+class HtSubmit extends React.Component {
+    submit = () => {
+        const { submitOk, useDate, xSquared, datiKey } = this.props;
+        if (submitOk) {
+            const time = (new Date().getTime() - useDate)/1000;
+            this.props.updateProps({
+                date: time,
+                value: true,
+            });
+            let id = 486;
+            const bool = typeof(window)=='undefined';
+            if(!bool){
+                id = window.location.pathname.match(/\/story\/(.*?)\//);
+            }
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "https://hi.amzport.com/api/data/queryAnswer", true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(JSON.stringify({
+                datiKey: `${datiKey}${id}`,
+                datiDate: [xSquared, time],
+            }));
+        } else {
+            alert("题目没答完");
+        }
+    };
+
+  render() {
+    const { hasError, idyll, updateProps, ...props } = this.props;
+    return (
+        <div {...props} onClick={this.submit}>
+            <span style={{
+                display: 'block',
+                borderRadius: '6px',
+                height: '45px',
+                lineHeight: '45px',
+                textAlign: 'center',
+                color: '#fff',
+                fontSize: '16px',
+                background: '#1ea0fa',
+                width: '200px',
+                margin: '25px auto',
+                textDecoration: 'none' }}>提交</span>
+        </div>
+    );
+  }
+}
+
+module.exports = HtSubmit;
---
+++ components/ht-test.js
@@ -0,0 +1,65 @@
+const React = require('react');
+
+class HtTest extends React.Component {
+    state = {
+        grade: 0,
+        times: 0,
+        num: 0,
+    };
+
+    componentDidMount() {
+        const datiKey = this.props.datiKey;
+        let id = 486;
+        const bool = typeof(window)=='undefined';
+        if(!bool){
+            id = window.location.pathname.match(/\/story\/(.*?)\//);
+        }
+        const that = this;
+        this.queryAnswer(function (jsCg) {
+            that.setState({
+                grade: jsCg.data[1],
+                times: jsCg.data[2],
+                num: jsCg.data[0],
+            });
+        },  `${datiKey}${id}`)
+    }
+
+    queryAnswer = (done, datiKey) => {
+       var xhr = new XMLHttpRequest();
+       xhr.open("POST", "https://hi.amzport.com/api/data/queryAnswer", true);
+       xhr.setRequestHeader("Content-Type", "application/json");
+       xhr.onreadystatechange=function(){
+           if (xhr.readyState === 4){
+               done(JSON.parse(xhr.responseText));
+           }
+       };
+       xhr.onerror = function(){
+           done('');
+       };
+       xhr.send(JSON.stringify({
+           datiKey,
+       }));
+    };
+
+  render() {
+    const { hasError, idyll, updateProps,  ...props } = this.props;
+    return (
+        <div {...props} style={{ display: 'flex', justifyContent: 'space-around', color: '#757575' }}>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                <h3>答题人次</h3>
+                <p>{this.state.num}次</p>
+            </div>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+              <h3>平均分数</h3>
+              <p>{this.state.grade}分</p>
+          </div>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                <h3>平均用时</h3>
+                <p>{this.state.times}秒</p>
+            </div>
+      </div>
+    );
+  }
+}
+
+module.exports = HtTest;
