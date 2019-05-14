e27ada3e3c8b408a400905940a3b8fae9d1b046c
mxh <1075018028@qq.com>
2019-05-14 16:02:51 +0800

    ...

--- components/ht-clcChat.js
+++ components/ht-clcChat.js
@@ -11,49 +11,59 @@ class HTClcchat extends React.Component {
         const list = [];
         const gainList = [];
         for(let i = 0 ; i<20; i+=1){
-            gainList.push(this.props[`gain${i + 1}`])
+            if(this.props[`gain${i + 1}`]){
+                gainList.push(this.props[`gain${i + 1}`])
+            }
         }
+        let laterIndex = 0;
+        let isApperTureCount = 0;
         isAppearList.map((item, index)=>{
             if(item){
-                if(index === 0){
-                    list.push({x: data[index].time, y: startMoney * 1})
-                }else {
-                    let money = startMoney;
-                    for(let j = 0;j<=index;j+=1){
-                        const baifenbi = isAppearList[j] ? gainList[j]:1;
-                        money = money*baifenbi;
-                    }
-                    list.push({x: data[index].time, y: money })
+                isApperTureCount++;
+                let money = startMoney;
+                for(let j = 0;j<index;j+=1){
+                    const baifenbi = isAppearList[j] ? gainList[j]:1;
+                    money = money*baifenbi;
                 }
+                laterIndex = index;
+                list.push({x: data[index].time, y: money })
             }
            return item;
         });
-
+        const time = this.props.time;
+        if(isApperTureCount !== 1){
+            list.push({x:time, y: list[list.length-1].y * gainList[laterIndex]});
+        }
         this.increment(list);
     }
     componentWillReceiveProps(nextProps){
         const {value, data, isAppearList, startMoney } = nextProps;
-        console.log(nextProps)
         const list = [];
         const gainList = [];
         for(let i = 0 ; i<20; i+=1){
-            gainList.push(nextProps[`gain${i + 1}`])
+            if(this.props[`gain${i + 1}`]){
+                gainList.push(this.props[`gain${i + 1}`])
+            }
         }
+        let laterIndex = 0;
+        let isApperTureCount = 0;
         isAppearList.map((item, index)=>{
             if(item){
-                if(index === 0){
-                    list.push({x: data[index].time, y: startMoney * 1})
-                }else {
+                    isApperTureCount++;
                     let money = startMoney;
-                    for(let j = 0;j<=index;j+=1){
+                    for(let j = 0;j<index;j+=1){
                         const baifenbi = isAppearList[j] ? gainList[j]:1;
                         money = money*baifenbi;
                     }
+                    laterIndex = index;
                     list.push({x: data[index].time, y: money })
-                }
             }
             return item;
         });
+        const time = nextProps.time;
+        if(isApperTureCount !== 1){
+            list.push({x:time, y: list[list.length-1].y * gainList[laterIndex]});
+        }
         this.increment(list);
     }
 
